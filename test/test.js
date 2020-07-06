f0ed80d9232dbe32b1a989eee227700e0491f1be
Vladimir Agafonkin <agafonkin@gmail.com>
2020-07-06 19:55:47 +0300

    upgrade deps, bump travis node

--- test/test.js
+++ test/test.js
@@ -93,21 +93,21 @@ test('robustness', (t) => {
 
 test('returns empty triangulation for small number of points', (t) => {
     let d = Delaunator.from([]);
-    t.same(d.triangles, []);
-    t.same(d.hull, []);
+    t.same(Array.from(d.triangles), []);
+    t.same(Array.from(d.hull), []);
     d = Delaunator.from(points.slice(0, 1));
-    t.same(d.triangles, []);
-    t.same(d.hull, [0]);
+    t.same(Array.from(d.triangles), []);
+    t.same(Array.from(d.hull), [0]);
     d = Delaunator.from(points.slice(0, 2));
-    t.same(d.triangles, []);
-    t.same(d.hull, [1, 0]); // [0, 1] is also correct
+    t.same(Array.from(d.triangles), []);
+    t.same(Array.from(d.hull), [1, 0]); // [0, 1] is also correct
     t.end();
 });
 
 test('returns empty triangulation for all-collinear input', (t) => {
     const d = Delaunator.from([[0, 0], [1, 0], [3, 0], [2, 0]]);
-    t.same(d.triangles, []);
-    t.same(d.hull, [0, 1, 3, 2]); // [2, 3, 0, 1] is also correct
+    t.same(Array.from(d.triangles), []);
+    t.same(Array.from(d.hull), [0, 1, 3, 2]); // [2, 3, 0, 1] is also correct
     t.end();
 });
 
@@ -116,7 +116,7 @@ test('supports custom point format', (t) => {
         [{x: 5, y: 5}, {x: 7, y: 5}, {x: 7, y: 6}],
         p => p.x,
         p => p.y);
-    t.same(d.triangles, [0, 2, 1]);
+    t.same(Array.from(d.triangles), [0, 2, 1]);
     t.end();
 });
 
