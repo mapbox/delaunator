e094737aea6d464a975ac0e9d8665a67127e5209
Amit Patel <redblobgames@gmail.com>
2025-06-30 05:30:27 -0700

    guide and readme - use complete examples with labels on points/edges (#88)
    Rationale: the example code in the README isn't complete, so it can't
    be run. Provide a complete example instead, with a visualization
    showing what it outputs.
    
    Use the same example in the Guide.
    
    Change the Guide example at the top of the page to show a complete
    example instead of displaying a subset of the output. This means we
    now show half-edges without an opposite, an important half-edge case
    that was previously not shown in the Guide.
    
    Add a diagram to the Guide showing the half-edge ids. Show the output
    of delaunay.triangles and delaunay.halfedges so that the reader can
    study the half-edge ids and point ids in the diagram to see how they
    correspond to the values in the output arrays.
    
    Be more explicit about which code works with "new Delaunator(coords)"
    and which code works with "Delaunator.from(points)". Use those names,
    "coords" and "points" consistently.
    
    Some of the examples here are fragile in the sense that
    docs/index.html makes assumptions about the examples in
    docs/diagrams.js. I've put <-- comments --> in those places as
    reminders.
    
    Fixes #83, #76

--- docs/diagrams.js
+++ docs/diagrams.js
@@ -14,12 +14,27 @@
 
 /* global Delaunator, triangleCenter, forEachTriangle, forEachTriangleEdge, forEachVoronoiCell, edgesAroundPoint, nextHalfedge */
 
-/* points1 = new Poisson([1000, 1000], 70, undefined, undefined, Math.random).fill() */
-const points1 = [[338, 601], [357, 469], [200, 583], [424, 634], [302, 516], [265, 650], [459, 570], [367, 723], [453, 454], [220, 472], [326, 367], [424, 369], [393, 544], [121, 661], [180, 704], [126, 516], [87, 588], [523, 614], [472, 730], [277, 751], [585, 554], [532, 497], [351, 835], [450, 819], [511, 377], [586, 429], [251, 396], [164, 416], [224, 322], [299, 238], [404, 275], [470, 315], [121, 762], [2, 630], [21, 699], [186, 828], [40, 412], [28, 496], [572, 699], [646, 616], [566, 811], [275, 856], [622, 493], [720, 565], [382, 945], [257, 933], [475, 895], [554, 890], [512, 255], [627, 367], [570, 314], [712, 448], [126, 337], [122, 265], [212, 216], [397, 183], [326, 154], [35, 833], [117, 848], [145, 935], [58, 296], [652, 769], [653, 686], [770, 664], [628, 884], [697, 846], [816, 549], [454, 980], [586, 971], [670, 968], [508, 119], [573, 179], [697, 247], [755, 324], [625, 264], [783, 442], [73, 178], [142, 186], [188, 123], [408, 98], [318, 68], [21, 948], [9, 207], [782, 761], [717, 730], [854, 729], [894, 659], [741, 916], [815, 896], [884, 580], [856, 416], [898, 511], [804, 996], [599, 69], [472, 10], [665, 138], [821, 232], [775, 147], [884, 279], [98, 53], [17, 94], [208, 28], [381, 5], [91, 980], [836, 811], [918, 766], [974, 706], [900, 843], [962, 580], [939, 930], [874, 969], [970, 492], [958, 327], [938, 405], [567, 0], [700, 16], [910, 184], [863, 106], [956, 248], [780, 21], [890, 354], [987, 801], [979, 96], [742, 82], [917, 44], [853, 14], [986, 876], [986, 171], [980, 11]];
-const points2 = [[320, 170], [400, 270], [220, 270], [530, 50], [100, 80], [300, 30]];
+/** Helper function to construct a subset of points for some of the diagrams */
+function filterToSvgRect(points, left, top, width, height) {
+    return points.filter(([x, y]) => left <= x && x <= left + width && top <= y && y <= top + height);
+}
 
+// These points originally came from new Poisson([1000, 1000], 70, undefined, undefined, Math.random).fill()
+// but then were tweaked slightly to make the diagrams easier to read
+const points1 = [[338, 601], [377, 479], [200, 583], [424, 634], [302, 516], [265, 650], [459, 570], [367, 723], [453, 434], [220, 472], [326, 387], [444, 359], [393, 544], [121, 661], [180, 704], [126, 516], [87, 588], [523, 614], [472, 730], [277, 751], [585, 554], [532, 507], [351, 835], [450, 819], [511, 389], [586, 429], [251, 404], [164, 416], [224, 322], [299, 238], [404, 275], [470, 315], [121, 762], [2, 630], [21, 699], [186, 828], [40, 412], [28, 496], [572, 699], [646, 616], [566, 811], [275, 856], [622, 493], [720, 565], [382, 945], [257, 933], [475, 895], [554, 890], [450, 215], [627, 367], [570, 314], [712, 448], [126, 337], [122, 265], [212, 216], [397, 183], [326, 154], [35, 833], [117, 848], [145, 935], [58, 296], [652, 769], [653, 686], [770, 664], [628, 884], [697, 846], [816, 549], [454, 980], [586, 971], [670, 968], [508, 119], [573, 179], [697, 247], [755, 324], [625, 264], [783, 442], [73, 178], [142, 186], [188, 123], [408, 98], [318, 68], [21, 948], [9, 207], [782, 761], [717, 730], [854, 729], [894, 659], [741, 916], [815, 896], [884, 580], [856, 416], [898, 511], [804, 996], [599, 69], [472, 10], [665, 138], [821, 232], [775, 147], [884, 279], [98, 53], [17, 94], [208, 28], [381, 5], [91, 980], [836, 811], [918, 766], [974, 706], [900, 843], [962, 580], [939, 930], [874, 969], [970, 492], [958, 327], [938, 405], [567, 0], [700, 16], [910, 184], [863, 106], [956, 248], [780, 21], [890, 354], [987, 801], [979, 96], [742, 82], [917, 44], [853, 14], [986, 876], [986, 171], [980, 11]];
 const delaunay1 = Delaunator.from(points1);
+
+// A smaller set of points for the diagrams at the top of the page
+const points2 = filterToSvgRect(points1, 100, 200, 800, 200);
 const delaunay2 = Delaunator.from(points2);
+
+// An even smaller set of points for the diagrams that show point/edge/triangle labels
+const points3 = filterToSvgRect(points1, 300, 300, 400, 200);
+const delaunay3 = Delaunator.from(points3);
+
+// A small set of points for the "circulation" diagram
+const points4 = [[320, 170], [400, 270], [220, 270], [530, 50], [100, 80], [300, 30]];
+const delaunay4 = Delaunator.from(points4);
 
 
 function tangent(a, b) {
@@ -42,6 +57,19 @@ function redPointsSvg(points) {
     for (let i = 0; i < points.length; i++) {
         const p = points[i];
         results.push(`<circle class="point-${i}" cx="${p[0]}" cy="${p[1]}" r="5"/>`);
+    }
+    results.push('</g>');
+    return results.join('');
+}
+
+function redPointsNumericSvg(points) {
+    const results = ['<g class="seed-labels">'];
+    for (let i = 0; i < points.length; i++) {
+        const p = points[i];
+        results.push(`<g class="point-${i}" transform="translate(${p})">
+                               <circle r="7"/>
+                               <text dy="3">${i}</text>
+                            </g>`);
     }
     results.push('</g>');
     return results.join('');
@@ -98,17 +126,21 @@ function cellsSvg(points, delaunay) {
     return results.join('');
 }
 
-function halfedgeSvg(points, delaunay, filter = () => true) {
-    const dn = 4, dt = 15;
+function halfedgeSvg(points, delaunay, options) {
+    const dn = 3, dt = 13, labelSpacing = 8;
     const results = ['<g class="edges arrowhead">'];
     for (let e1 = 0; e1 < delaunay.halfedges.length; e1++) {
-        if (!filter(e1)) { continue; }
+        if (options.filter && !options.filter(e1)) { continue; }
         const e2 = nextHalfedge(e1);
         const a = points[delaunay.triangles[e1]];
         const b = points[delaunay.triangles[e2]];
         const n = normal(a, b);
         const t = tangent(a, b);
-        results.push(`<line class="edge-${e1}" x1="${a[0] + dn * n[0] + dt * t[0]}" y1="${a[1] + dn * n[1] + dt * t[1]}" x2="${b[0] + dn * n[0] - dt * t[0]}" y2="${b[1] + dn * n[1] - dt * t[1]}" />`);
+        results.push(`<line class="edge-${e1}" x1="${a[0] + dn * n[0] + dt * t[0]}" y1="${a[1] + dn * n[1] + dt * t[1]}"
+                                                     x2="${b[0] + dn * n[0] - dt * t[0]}" y2="${b[1] + dn * n[1] - dt * t[1]}" />`);
+        if (options.labels) {
+            results.push(`<text x="${(a[0] + b[0]) / 2 + labelSpacing * n[0]}" y="${(a[1] + b[1]) / 2 + labelSpacing * n[1]}" dy="2.5">${e1}</text>`);
+        }
     }
     results.push('</g>');
     return results.join('');
@@ -119,23 +151,80 @@ const $ = document.querySelector.bind(document);
 
 $('#diagram-points').innerHTML = `
 <svg viewBox="100 200 800 200">
-  ${redPointsSvg(points1)}
+  ${redPointsSvg(points2)}
 </svg>
 <figcaption>Delaunator input</figcaption>`;
 
 
 $('#diagram-delaunay').innerHTML = `
 <svg viewBox="100 200 800 200">
-  ${delaunaySvg(points1, delaunay1)}
-  ${redPointsSvg(points1)}
+  ${delaunaySvg(points2, delaunay2)}
+  ${redPointsSvg(points2)}
 </svg>
 <figcaption>Delaunator output</figcaption>`;
 
 
+/** format points */
+function formatPointArray(points, {prefix, suffix, pointsPerLine}) {
+    let output = '';
+    let i = 0;
+    while (true) {
+        output += `[${points[i]}]`;
+        i++;
+        if (i === points.length) break;
+        output += ', ';
+        if (i % pointsPerLine === 0) output += '\n'.padEnd(prefix.length + 2);
+    }
+    return `${prefix}[${output}]${suffix}`;
+}
+
+$('#diagram-point-labels').innerHTML = `
+<svg viewBox="300 300 400 200">
+  ${redPointsNumericSvg(points3)}
+</svg>
+<pre>${formatPointArray(points3, {prefix: 'const points = ', suffix: ';', pointsPerLine: 5})}
+const delaunay = Delaunator.from(points);</pre>
+</pre>
+<figcaption>Delaunator input is an array of points</figcaption>`;
+
+/** format delaunay.triangles in groups of 3 indices - works for our needs but not in general */
+function formatTriangleArray(triangles, {prefix, trianglesPerLine}) {
+    let output = '';
+    let i = 0;
+    while (true) {
+        output += triangles.slice(i, i + 3);
+        i += 3;
+        if (i === triangles.length) break;
+        output += ',  ';
+        if ((i / 3) % trianglesPerLine === 0) output += '\n'.padEnd(prefix.length + 2);
+    }
+    return `${prefix}[${output}]`;
+}
+
+$('#diagram-delaunay-labels').innerHTML = `
+<svg viewBox="300 300 400 200">
+  ${delaunaySvg(points3, delaunay3)}
+  ${redPointsNumericSvg(points3)}
+</svg>
+<pre>${formatTriangleArray(delaunay3.triangles, {prefix: 'delaunay.triangles == ', trianglesPerLine: 6})}</pre>
+<figcaption>Delaunator output</figcaption>`;
+
+
+// TODO: the half-edge arrows in this diagram overlap slightly, and it would be nicer
+// if they didn't. Since we're not making a general purpose layout algorithm we could
+// put in some tweaks for this specific dataset.
 $('#diagram-halfedges').innerHTML = `
 <svg viewBox="300 300 400 200">
-  ${halfedgeSvg(points1, delaunay1)}
-  ${redPointsSvg(points1)}
+  ${halfedgeSvg(points3, delaunay3, {})}
+  ${redPointsSvg(points3)}
+</svg>
+<figcaption>Half-edges</figcaption>`;
+
+
+$('#diagram-halfedges-labels').innerHTML = `
+<svg viewBox="300 300 400 200">
+  ${halfedgeSvg(points3, delaunay3, {labels: true})}
+  ${redPointsNumericSvg(points3)}
 </svg>
 <figcaption>Half-edges</figcaption>`;
 
@@ -182,18 +271,18 @@ $('#diagram-voronoi').innerHTML = `
 $('#diagram-circulate').innerHTML = `
 <svg viewBox="0 0 600 300">
   <g class="highlight"></g>
-  ${halfedgeSvg(points2, delaunay2)}
-  ${redPointsSvg(points2)}
+  ${halfedgeSvg(points4, delaunay4, {})}
+  ${redPointsSvg(points4)}
 </svg>
 Step: <input type=range value=0 min=0 max=9 oninput="updateCirculation()">
 <figcaption>Circulate around point</figcaption>
 </figure>`;
 function updateCirculation() {
     const slider = $('#diagram-circulate input');
-    const incoming = edgesAroundPoint(delaunay2, 2);
+    const incoming = edgesAroundPoint(delaunay4, 2);
     const outgoing = incoming.map(e => nextHalfedge(e));
     const step = slider.valueAsNumber;
     const edge = step % 2 === 0 ? incoming[step / 2] : outgoing[(step - 1) / 2];
-    $('#diagram-circulate .highlight').innerHTML = halfedgeSvg(points2, delaunay2, e => e === edge);
+    $('#diagram-circulate .highlight').innerHTML = halfedgeSvg(points4, delaunay4, {filter: e => e === edge});
 }
 updateCirculation();
